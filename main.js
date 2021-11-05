3ecd86ee6077ead4a9c33f88ab0f15572a97910a
SD3K <skipdawg3000@gmail.com>
2021-11-05 16:24:49 -0700

    added randomization to get functions

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 function getDestination(){
-  //
+  // generates a random destination for your day trip
   let destinations = ['dest1', 'dest2', 'dest3', 'dest4'];
   let selection = destinations[Math.floor(Math.random() * (destinations.length))];
   return selection;
 };
 
 function getTransportaion(){
-  //
+  // generates a random mode of transportation to arrive at your day trip destination
   let transport = ['mode1', 'mode2', 'mode3', 'mode4'];
+  let selection = transport[Math.floor(Math.random() * (destinations.length))];
+  return selection;
 };
 
 function getFood(){
-  //
-  let food = ['food1', 'food2', 'food3', 'food4'];
+  // generates a random food to eat during your day trip
+  let  = ['food1', 'food2', 'food3', 'food4'];
+  let selection = food[Math.floor(Math.random() * (destinations.length))];
+  return selection;
 };
 
 function getFun(){
-  //
+  // generates a random activity for fun on your day trip
   let fun = ['fun1', 'fun2', 'fun3', 'fun4'];
+  let selection = fun[Math.floor(Math.random() * (destinations.length))];
+  return selection;
 };
 
 function generateTrip(){
