6127950902f24f5205442d0aaed50b70f1ed7fe0
Mike Peterson <mpeterson2004@gmail.com>
2021-11-05 17:09:08 -0600

    commiting my version to his function teach about merge conflicts

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function getDestination(){
-  //
-  let destinations = ['dest1', 'dest2', 'dest3', 'dest4'];
-};
+"use strict";
+function getDestination(previousDestination=""){
+    //this function should select a destination at random from sight seeing spots around Utah
+        let location = ["Temple Square", "Big Cottonwood Canyon", "Lagoon","Pineview", "Timpanogos Cave","Stay Home"]
+        let result = location[Math.floor(Math.random()*location.length)];
+        //if user has called function previously control to not return the same result
+        while (previousDestination == result){
+            result = location[Math.floor(Math.random()*location.length)]; 
+    
+        }
+        return result;
+    }
 
 function getTransportaion(){
   //
